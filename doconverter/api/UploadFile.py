04edc4759e1e31c221455018f18140bfe7ae3131
gasparuben <gasparuben@gmail.com>
2017-01-16 18:14:49 +0100

    Copyright license date, update doconverter.ini with database|

--- doconverter/api/UploadFile.py
+++ doconverter/api/UploadFile.py
@@ -1,7 +1,7 @@
 #!c:\Python34\python.exe
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2016, CERN
+# Copyright (C) 2017, CERN
 # This software is distributed under the terms of the GNU General Public
 # Licence version 3 (GPL Version 3), copied verbatim in the file "LICENSE".
 # In applying this license, CERN does not waive the privileges and immunities

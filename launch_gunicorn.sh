185d9995520ac171baf920de5dc84c1a24a3933e
gasparuben <gasparuben@gmail.com>
2017-08-01 19:19:41 +0200

    parameters in order to launch gunicorn4

--- launch_gunicorn.sh
+++ launch_gunicorn.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
+
 cd ./doconverter/api/
+
+if [[ -z $TIMEOUTWORKERS ]]; then
+    echo "TIMEOUTWORKERS variable not set. Setting to 200"
+    TIMEOUTWORKERS=200
+fi
+if [[ -z $WORKERS ]]; then
+    echo "WORKERS variable not set. Setting to 1"
+    WORKERS=1
+fi
 gunicorn handlers:app -p handlers.pid -b 0.0.0.0:8080 -t $TIMEOUTWORKERS -w $WORKERS

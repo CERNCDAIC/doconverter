e1f65aec4c5a8f2dd32a7e6c72c99a7e495b4eb6
Conversion service <conversion.service@cern.ch>
2017-09-29 11:05:22 +0200

    + need to set path for module since last Openshift update

--- launch_gunicorn.sh
+++ launch_gunicorn.sh
@@ -10,4 +10,4 @@ if [[ -z $WORKERS ]]; then
     echo "WORKERS variable not set. Setting to 1"
     WORKERS=1
 fi
-gunicorn handlers:app -p handlers.pid -b 0.0.0.0:8080 -t $TIMEOUTWORKERS -w $WORKERS
+gunicorn handlers:app -p handlers.pid -b 0.0.0.0:8080 --pythonpath $HOME -t $TIMEOUTWORKERS -w $WORKERS
